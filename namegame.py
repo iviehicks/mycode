7654b95e7d8ec651b059cf33e56ca12bdbeeb007
iviehicks <ivie.hicks@gmail.com>
2024-02-06 22:53:48 +0000

    manipulating lists

---
+++ namegame.py
@@ -0,0 +1,8 @@
+#this is asking for a name
+name = input('What is your name?')
+
+#this is asking for the day
+day = input('What day is it?')
+
+#This prints out using f-string; one string with {} to add the variables
+print(f'Hello, {name}! Happy {day}!')
